3cf1185f5ea9b380fa391577fd8dd4bf9a074f99
jehadsway-design <jehadsway@gmail.com>
2026-07-25 00:55:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildModeration
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent
